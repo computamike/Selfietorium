a3de63fcfd7e707de24b68bef6e1d3d4624af056
Mike Hingley <computa_mike@hotmail.com>
2016-03-06 14:02:30 +0000

    Getting Display SVG to work

--- Proof Of Concept Applications/displaySVG.py
+++ Proof Of Concept Applications/displaySVG.py
@@ -6,28 +6,71 @@ import math
 import cairo
 import pygame
 import rsvg
+import PIL.Image
 
-WIDTH = 512
-HEIGHT = 512
+WIDTH = 640
+HEIGHT = 480
 
-data = array.array('c', chr(0) * WIDTH * HEIGHT * 4)
-surface = cairo.ImageSurface.create_for_data(
-    data, cairo.FORMAT_ARGB32, WIDTH, HEIGHT, WIDTH * 4)
+def load_svg(filename):
+    def bgra_rgba(surface):
+        img = PIL.Image.frombuffer('RGBA',(surface.get_width(),surface.get_height()),surface.get_data(),'raw','RGBA',0,1)
+        return img.tostring('raw','RGBA',0,1)
+    svg = rsvg.Handle(filename)
+
+    img_w,img_h = svg.props.width,svg.props.height
+
+    ScaleFactorx = float(WIDTH) / float(img_w)
+    ScaleFactory = float(HEIGHT) / float(img_h)
+
+
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,WIDTH,HEIGHT)
+    ctx = cairo.Context(surface)
+    ctx.scale(ScaleFactorx,ScaleFactory)
+
+    svg.render_cairo(ctx)
+    return pygame.image.frombuffer(bgra_rgba(surface),(WIDTH,HEIGHT),'RGBA')
+                                   
+
+
+
+
+
+
 
 pygame.init()
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-svg = rsvg.Handle(file="test.svg")
-ctx = cairo.Context(surface)
-svg.render_cairo(ctx)
+pygame.display.set_mode((WIDTH,HEIGHT))
+background = pygame.Surface((WIDTH,HEIGHT))
+background = background.convert()
+background.fill((0,0,0))
 
-screen = pygame.display.get_surface()
-image = pygame.image.frombuffer(data.tostring(), (WIDTH, HEIGHT),"ARGB")
-screen.blit(image, (0, 0)) 
-pygame.display.flip() 
 
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+c=pygame.time.Clock()
 while True:
-    clock.tick(15)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            raise SystemExit
+    IMG = load_svg("../Attract.svg")
+    screen.blit(background,(0,0))
+    screen.blit(IMG,(0,0))
+    pygame.display.flip()
+    c.tick(1)
+
+##data = array.array('c', chr(0) * WIDTH * HEIGHT * 4)
+##surface = cairo.ImageSurface.create_for_data(
+##    data, cairo.FORMAT_ARGB32, WIDTH, HEIGHT, WIDTH * 4)
+##
+##pygame.init()
+##window = pygame.display.set_mode((WIDTH, HEIGHT))
+##svg = rsvg.Handle(file="../Attract.svg")
+##ctx = cairo.Context(surface)
+##svg.render_cairo(ctx)
+##
+##screen = pygame.display.get_surface()
+##image = pygame.image.frombuffer(data.tostring(), (WIDTH, HEIGHT),"ARGB")
+##screen.blit(image, (0, 0)) 
+##pygame.display.flip() 
+##
+##clock = pygame.time.Clock()
+##while True:
+##    clock.tick(15)
+##    for event in pygame.event.get():
+##        if event.type == pygame.QUIT:
+##            raise SystemExit
--- bailey.py
+++ bailey.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
-
+import sys
 import RPi.GPIO as GPIO
 import pygame
+import time
 from pygame.locals import *
-
-def keyPressed(inputkey):
-    keysPressed = key.get_pressed()
-    if keysPressed[inputkey]:
-        return True
-    else:
-        return False
 
 
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(7, GPIO.OUT) 
+GPIO.setup(15, GPIO.OUT) 
+GPIO.setup(12, GPIO.OUT) 
+GPIO.setup(18, GPIO.OUT) 
+
+
 # 7 is the for LED1. You need to do this for each LED, or use lists, etc
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+
 # 21 is the for the Red button. You need to do this for each button, or use lists, etc
+
 
 pygame.init()
 infoObject = pygame.display.Info()
@@ -33,6 +37,7 @@ img = pygame.transform.scale(img,(WIDTH,HEIGHT))
 
 
 
+
 running = True
 
 while running:
@@ -44,10 +49,37 @@ while running:
         elif event.type==KEYDOWN:
             if event.key == K_ESCAPE :
                 running = False
-        if GPIO.input(21):
-            GPIO.output(7,1)
-        else:
-            GPIO.output(7,0)
-        windowSurface.blit(img, (0, 0)) #Replace (0, 0) with desired coordinates
-        pygame.display.flip()
+
+    if GPIO.input(24):
+        GPIO.output(15,1)
+        #pygame.quit()
+        #sys.exit()
+    else:
+        GPIO.output(15,0)
+    
+
+    if GPIO.input(26):
+        GPIO.output(18,1)
+        #pygame.quit()
+        #sys.exit()
+    else:
+        GPIO.output(18,0)
+
+
+    if GPIO.input(19):
+        GPIO.output(12,1)
+        #pygame.quit()
+        #sys.exit()
+    else:
+        GPIO.output(12,0)
+
+
+
+
+    if GPIO.input(21):
+        GPIO.output(7,1)
+    else:
+        GPIO.output(7,0)
+    windowSurface.blit(img, (0, 0)) #Replace (0, 0) with desired coordinates
+    pygame.display.flip()
 pygame.quit()
