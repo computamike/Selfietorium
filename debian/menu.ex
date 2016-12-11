f4ebe76e5c222e9c3d3b69614d943334140ed903
Mike Hingley <computa_mike@hotmail.com>
2016-12-11 11:59:15 +0000

    Adding debian folder

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(selfietorium):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="selfietorium" command="/usr/bin/selfietorium"
---
+++ debian/selfietorium.default.ex
@@ -0,0 +1,10 @@
+# Defaults for selfietorium initscript
+# sourced by /etc/init.d/selfietorium
+# installed at /etc/default/selfietorium by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
